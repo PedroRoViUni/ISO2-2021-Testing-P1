823d6c3b5c3fe997a60b19ec7c82c92ade8216fd
Alejandro Villamayor Zapero <91552023+ZaP3R0@users.noreply.github.com>
2021-12-23 16:58:51 +0100

    Añadido proyecto maven

--- E1-C01/src/main/java/C01_ISO2/E1_C01/E1_ISO2.java
+++ E1-C01/src/main/java/C01_ISO2/E1_C01/E1_ISO2.java
@@ -3,8 +3,23 @@ package C01_ISO2.E1_C01;
 public class E1_ISO2 {
 	public static void main(String [] args) {
 		Persona a1 = new Persona(true, false, false, true, true);
+		Persona a2 = new Persona(true, true, true, true, true);
+		Persona a3 = new Persona(true, false, false, true, false);
+		Persona a4 = new Persona(true, false, false, false, true);
+		Persona a5 = new Persona(false, false, false, true, true);
 		
 		realizarActividad(12, 12, "Nieve", a1);
+		realizarActividad(17, 32, "Nieve", a2);
+		realizarActividad(28, 30, "Granizo", a3);
+		realizarActividad(35, 70, "Niebla", a4);
+		realizarActividad(-14, 12, "Granizo", a5);
+		
+		realizarActividad(16, 20, "Nieve", a1);
+		realizarActividad(-2, 12, "Granizo", a1);
+		realizarActividad(32, 15, "Nieve", a1);
+		realizarActividad(35, 15, "Granizo", a1);
+		realizarActividad(-5, 14, "Lluvia", a1);
+				
 	}
 	
 	public static void realizarActividad(int temperatura, double humedad, String precipitacion, Persona p) {
